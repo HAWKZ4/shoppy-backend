7066124f980d5884afebc298a9c567be0f083122
M-A <108879264+HAWKZ4@users.noreply.github.com>
2025-10-12 19:08:41 +0300

    chore(logging): setup global Pino logger

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useLogger(app.get(Logger))
   await app.listen(app.get(ConfigService).getOrThrow('PORT'));
 }
 bootstrap();
