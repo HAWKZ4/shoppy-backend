41b604ca82fee46189438861a0668301ab3950b6
M-A <108879264+HAWKZ4@users.noreply.github.com>
2025-10-14 20:17:21 +0300

    fix(users): add NoFilesInterceptor and return statement in createUser method

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseInterceptors } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserRequest } from './dto/create-user.request';
+import { NoFilesInterceptor } from '@nestjs/platform-express';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
   @Post()
+  @UseInterceptors(NoFilesInterceptor())
   async createUser(@Body() request: CreateUserRequest) {
-    await this.usersService.createUser(request);
+    return this.usersService.createUser(request);
   }
 }
