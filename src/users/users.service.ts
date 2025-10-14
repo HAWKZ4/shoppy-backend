a1a235a4829dffefd56b7baa473d62f53a5eb282
M-A <108879264+HAWKZ4@users.noreply.github.com>
2025-10-14 13:51:50 +0300

    feat(users): add create user endpoint with validation DTO

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserRequest } from './dto/create-user.request';
+import { User } from 'generated/prisma';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+  async createUser(data: CreateUserRequest) {
+    try {
+      return await this.prismaService.user.create({
+        data: {
+          ...data,
+          password: await bcrypt.hash(data.password, 10),
+        },
+        select: {
+          id: true,
+          email: true,
+        },
+      });
+    } catch (err) {
+      console.error(err);
+      if (err.code === 'P2002') {
+        throw new ConflictException('Email already exists');
+      }
+      throw err;
+    }
+  }
+}
