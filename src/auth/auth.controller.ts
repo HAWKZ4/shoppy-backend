0104400ad50af6e77a9d50b42d776b723c0f0949
M-A <108879264+HAWKZ4@users.noreply.github.com>
2025-10-16 20:41:11 +0300

    feat(auth): implement login endpoint and CurrentUser decorator
    - Add login controller/service methods, issue JWT token, and send it as HTTP-only cookie
    - Configure JwtModule asynchronously using ConfigModule and enviroment variables
    - Create TokenPayload interface for typing JWT payload

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Post, UseGuards } from '@nestjs/common';
-import { UsersService } from 'src/users/users.service';
+import { Controller, Post, Res, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+import { CurrentUser } from './current-user.decorator';
+import type { User } from '@prisma/client';
+import type { Response } from 'express';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
-  async login() {}
+  async login(
+    @CurrentUser() user: User,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    return this.authService.login(user, response);
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,47 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
+import ms from 'ms';
+import { User } from '@prisma/client';
+import { TokenPayload } from './token-payload';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(user: User, response: Response) {
+    const expires = new Date();
+    expires.setMilliseconds(
+      expires.getMilliseconds() +
+        ms(
+          this.configService.getOrThrow<string>(
+            'JWT_EXPIRATION',
+          ) as unknown as ms.StringValue,
+        ),
+    );
+
+    const tokenPayload: TokenPayload = {
+      userId: user.id,
+    };
+
+    const token = this.jwtService.sign(tokenPayload);
+
+    response.cookie('Authentication', token, {
+      httpOnly: true,
+      secure: true,
+      expires,
+    });
+
+    return { tokenPayload };
+  }
+
   async verifyUser(email: string, password: string) {
     try {
       const user = await this.usersService.getUser({ email });
