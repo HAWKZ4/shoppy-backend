02af584b0f2b52b41a58a735356f9878d1349cd0
M-A <108879264+HAWKZ4@users.noreply.github.com>
2025-10-22 09:37:11 +0300

    feat(products): add product image upload endpoint with validation

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,9 +1,23 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  FileTypeValidator,
+  Get,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductRequest } from './dto/create-product.request';
 import { CurrentUser } from 'src/auth/current-user.decorator';
 import type { TokenPayload } from 'src/auth/token-payload';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 @Controller('products')
 export class ProductsController {
@@ -17,6 +31,33 @@ export class ProductsController {
   ) {
     return this.productsService.createProduct(body, user.userId);
   }
+
+  @Post(':productId/image')
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: 'public/products',
+        filename: (req, file, callback) => {
+          callback(
+            null,
+            `${req.params.productId}${extname(file.originalname)}`,
+          );
+        },
+      }),
+    }),
+  )
+  async uploadProductImage(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 5 * 1024 * 1024 }),
+          new FileTypeValidator({ fileType: 'image/jpeg' }),
+        ],
+      }),
+    )
+    _file: Express.Multer.File,
+  ) {}
 
   @Get()
   @UseGuards(JwtAuthGuard)
