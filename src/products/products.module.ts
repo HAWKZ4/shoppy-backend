77f4a4f5d0932003df3fcc0742e719dbda3c5947
M-A <108879264+HAWKZ4@users.noreply.github.com>
2025-10-25 18:30:59 +0300

    feat(ws): secure WebSocket connections with token verification
    - Implement handleConnection in ProductsGateway to authenticate users
    - Add verifyToken method in AuthService for validating JWT tokens

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -3,11 +3,12 @@ import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { ProductsGateway } from './products.gateway';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [ProductsController],
-  providers: [ProductsService,ProductsGateway],
+  providers: [ProductsService, ProductsGateway],
   exports: [ProductsService],
 })
 export class ProductsModule {}
